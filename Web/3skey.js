abab30bbe568bd8414ee62d8cf18819d49105dd6
Ahmad Moussawi <aymoussawi@gmail.com>
2016-04-27 17:03:11 +0300

    add the option to include the header and footer inside the js kit

--- Web/3skey.js
+++ Web/3skey.js
@@ -5,17 +5,17 @@
  *  @author Ahmad Moussawi <ahmad.moussawi@treasuryxpress.com>
  * 
  */
-; (function(global) {
+; (function (global) {
     function Token3Skey(applet) {
         this.applet = applet;
         this.slot = null;
     }
 
-    Token3Skey.prototype.setSlot = function(slot) {
+    Token3Skey.prototype.setSlot = function (slot) {
         this.slot = slot;
         return this;
     }
-    
+
     /**
      * Generate a PKCS7 Attached signature (the file content will be embeded in the signature)
      * 
@@ -24,15 +24,15 @@
      * @onError function a fallback `function (err: string, code: string): void`
      * 
      */
-    Token3Skey.prototype.sign = function(content, onSuccess, onFailure) {
+    Token3Skey.prototype.sign = function (content, onSuccess, onFailure, withHeaderAndFooter) {
         return this.applet.sign(this.slot, content, {
-            onSuccess: function(response) {
-                onSuccess(["-----BEGIN PKCS7-----", response, "-----END PKCS7-----"].join('\n'));
+            onSuccess: function (response) {
+                onSuccess(!!withHeaderAndFooter ? attachHeaderAndFooter(response) : response);
             },
             onFailure: onFailure
         });
     }
-    
+
     /**
      * Generate a PKCS7 Detach signature (the file content will not be embeded in the signature)
      * 
@@ -41,20 +41,25 @@
      * @onError function a fallback `function (err: string, code: string): void`
      * 
      */
-    Token3Skey.prototype.signDetach = function(digest, onSuccess, onFailure) {
+    Token3Skey.prototype.signDetach = function (digest, onSuccess, onFailure, withHeaderAndFooter) {
         return this.applet.signDetachSignature(this.slot, digest, {
-            onSuccess: function(response) {
-                onSuccess(["-----BEGIN PKCS7-----", response, "-----END PKCS7-----"].join('\n'));
+            onSuccess: function (response) {
+                onSuccess(!!withHeaderAndFooter ? attachHeaderAndFooter(response) : response);
             },
             onFailure: onFailure
         });
     }
 
-    Token3Skey.prototype.logout = function(onSuccess, onFailure) {
+    Token3Skey.prototype.logout = function (onSuccess, onFailure) {
         return this.applet.logout(this.slot, {
             onSuccess: onSuccess,
             onFailure: onFailure
         })
     }
+
+    function attachHeaderAndFooter(content) {
+        return ["-----BEGIN PKCS7-----", content, "-----END PKCS7-----"].join('\n');
+    }
+
     global.Token3Skey = Token3Skey;
 })(window);
